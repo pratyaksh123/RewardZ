5d396d24be669d94ea5ad1ed83504230dcd706b7
Pratyaksh Tyagi <55044774+pratyaksh123@users.noreply.github.com>
2020-12-13 04:12:41 +0530

    Added RN-NetInfo

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Alert } from 'react-native';
 import Navigator from './App/Navigator/Stack';
 import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import NetInfo from '@react-native-community/netinfo';
 
 const theme = {
   ...DefaultTheme,
@@ -8,6 +10,18 @@ const theme = {
 
 // Wrap Here with a HOC if required.
 export default () => {
+  useEffect(() => {
+    NetInfo.addEventListener((state) => {
+      if (state.isConnected) {
+        console.log('Connected!');
+      } else {
+        Alert.alert(
+          'No Internet Connection Detected !',
+          'Please connect to the internet to access the App !'
+        );
+      }
+    });
+  }, []);
   return (
     <PaperProvider theme={theme}>
       <Navigator />
